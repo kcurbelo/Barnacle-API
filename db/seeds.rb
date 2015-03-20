3b01e446ec31cde21211339aa46e1674af9e87dc
kcurbelo <kevin.curbelo@gmail.com>
2015-03-20 15:25:18 -0700

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Beach.create(name: "", phone:"", url:"", description:"", address_street:"", address_city:"", address_state:"", address_zip:"", county:"", latitude:"", longitude:"", parking:"", parking_price:"", lifeguard:"", restroom:"", surfing:"", firepit:"", volleyball:"", dog:"", fishing:"" )
+Beach.create(name: "Prismo state beach", phone:"(805) 473-7220", url:"http://www.parks.ca.gov/?page_id=595", description:"Pismo State Beach offers all kinds of attractions: hiking, swimming, surf fishing, and digging for the famous Pismo clam. There are tree-lined dunes and the beach is popular with bird watchers. The park has the largest over-wintering colony of monarch butterflies in the U.S.", address_street:"555 Pier Avenue", address_city:"Oceano", address_state:"CA", address_zip:"93445", county:"San Luis Obispo", latitude:"35.106779", longitude:"-120.6252014", parking: true , parking_price:"10.00", lifeguard: true, restroom: true , surfing: true, firepit: false, volleyball: false, dog: true, fishing: false)
