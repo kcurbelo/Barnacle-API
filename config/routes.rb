1569a8497217e0d4b258e59d502a66c44e07b8fa
kcurbelo <kevin.curbelo@gmail.com>
2015-03-20 15:52:30 -0700

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :beaches
 
-  'beaches#index'
+  root 'beaches#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
